a0dab44052737a0e23b5180f84126d214bd8e6a9
revectores <revectores@gmail.com>
2020-10-16 20:02:13 +0800

    add cli tool

--- src/vector_clock/controller/controller.go
+++ src/vector_clock/controller/controller.go
@@ -1,13 +1,111 @@
-package main
+package controller
 
 import (
+	"os"
 	"fmt"
 	"log"
 	"net/rpc"
+	"encoding/json"
+	"io/ioutil"
 )
 
+
+type Addr struct {
+	IP [4]uint8
+	Port uint16
+}
+
+var Id2addr map[int]Addr
+
+
+func LoadConfigure() {
+	id2addrJSON, _ := ioutil.ReadFile("../../conf/id2addr.json")
+	json.Unmarshal(id2addrJSON, &Id2addr)
+	fmt.Println(Id2addr)
+}
+
+
+
+func getAddrString(id int) string {
+	addr := Id2addr[id]
+	addrString := fmt.Sprintf("%d.%d.%d.%d:%d", addr.IP[0], addr.IP[1],
+							 addr.IP[2], addr.IP[3], addr.Port)
+	return addrString
+}
+
+
+
+func rpcDial(id int) *rpc.Client {
+	addr := getAddrString(id)
+	client, err := rpc.Dial("tcp", addr)
+
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	
+	return client
+}
+
+
+func Init(id int){
+	client := rpcDial(id)
+
+	var i int
+	err := client.Call("NodeService.Init", Id2addr, &i)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(i)
+}
+
+
+func UpdateState(id int){
+	client := rpcDial(id)
+
+	var i int
+	err := client.Call("NodeService.UpdateState", 0, &i)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(i)
+}
+
+
+func SendMessage(senderId int, receiverId int) {
+	client := rpcDial(senderId)
+
+	var i int
+	err := client.Call("NodeService.SendMessage", receiverId, &i)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(i)
+}
+
+
+func GetVector(id int) {
+	client := rpcDial(id)
+
+	var clockVector []int;
+	err := client.Call("NodeService.GetVector", 0, &clockVector)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(clockVector)
+}
+
+
 func main() {
-	client, err := rpc.Dial("tcp", "localhost:1234")
+	client, err := rpc.Dial("tcp", "localhost:" + os.Args[1])
+	fmt.Println(os.Args[1])
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
--- src/vector_clock/node/node.go
+++ src/vector_clock/node/node.go
@@ -4,77 +4,162 @@ import (
 	"log"
 	"net"
 	"net/rpc"
+	"os"
+	"fmt"
+	"strconv"
 )
 
-type NodeService struct{}
-
-var vector_clock []int
-var cur_index int
-
-type InitTuple struct {
-	NodeCount int
-	NodeIndex int
+type Addr struct {
+	IP [4]uint8
+	Port uint16
 }
 
-func max(int a, int b) int {
+var Id2addr map[int]Addr
+
+
+var port uint16
+var clockVector []int
+var thisId int
+var initialized bool
+var nodeMode int
+
+var LOCALHOST = [4]uint8{127, 0, 0, 1}
+const DEV_MODE = 0
+const PROD_MODE = 1
+
+
+func max(a int, b int) int {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func vmax(int vec1[], int vec2[]) int[] {
-
-	if vec1.len() {
-  		panic(err)
+func vmax(vec1 []int, vec2 []int) []int {
+	if len(vec1) != len(vec2) {
+		panic("")
 	}
 
-	for i := range vec1.len()
+	updatedVector := make([]int, len(vec1))
 
+	for i := 0; i < len(vec1); i++ {
+		if vec1[i] > vec2[i] {
+			updatedVector[i] = vec1[i]
+		} else {
+			updatedVector[i] = vec2[i]
+		}
+	}
+	
+	return updatedVector
 }
 
 
+func getAddrString(id int) string {
+	addr := Id2addr[id]
+	addrString := fmt.Sprintf("%d.%d.%d.%d:%d", addr.IP[0], addr.IP[1],
+							 addr.IP[2], addr.IP[3], addr.Port)
+	return addrString
+}
+
+func rpcDial(id int) *rpc.Client {
+	addr := getAddrString(id)
+	client, err := rpc.Dial("tcp", addr)
+
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	
+	return client
+}
+
+
+type NodeService struct{}
 
 func (p *NodeService) Hello(request string, reply *string) error {
 	*reply = "hello:" + request
 	return nil
 }
 
-func (p *NodeService) InitVector(initTuple InitTuple, reply *int) error {
-	vector_clock, err = make([]int, InitTuple.NodeCount)
-	cur_index = InitTuple.NodeIndex
+func (p *NodeService) Init(id2addr map[int]Addr, reply *int) error {
+	Id2addr = id2addr
+	clockVector = make([]int, len(id2addr))
 
-	*reply = 0
+	nodeMode = DEV_MODE
+	for _, addr := range id2addr {
+		if addr.IP != LOCALHOST {
+			nodeMode = PROD_MODE
+			break
+		}
+	}
+
+	switch nodeMode {
+	case DEV_MODE:
+		thisId = -1
+		for id, addr := range id2addr {
+			if addr.Port == port {
+				thisId = id
+				break
+			}
+		}
+	case PROD_MODE:
+		// TOOD: PROD_MODE
+	}
+
+	*reply = thisId
+	return nil
+}
+
+func (p *NodeService) GetVector(_ int, reply *[]int) error {
+
+	*reply = clockVector
 	return nil
 }
 
 func (p *NodeService) UpdateState(_ int, reply *int) error {
-	vector_clock[cur_index]++
+	clockVector[thisId]++
 
 	*reply = 0
 	return nil
 }
 
-func (p *NodeService) Message(vector_msg int[], reply *int) error {
-
+func (p *NodeService) Message(clockVectorMsg []int, reply *int) error {
+	clockVector[thisId]++
+	clockVector = vmax(clockVector, clockVectorMsg)
 
 	*reply = 0
 	return nil
 }
 
-func (p *NodeService) SendMessage(_ int, reply *int) error {
-	vector_clock[cur_index]++
 
-	*reply = 0
+func (p *NodeService) SendMessage(id int, reply *int) error {
+	client := rpcDial(id)
+	
+	var i int;
+	err := client.Call("NodeService.Message", clockVector, &i)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	*reply = 0;
 	return nil
 }
 
 
 
 func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("Usage: ./node [port]")
+		return;
+	}
+
 	rpc.RegisterName("NodeService", new(NodeService))
 
-	listener, err := net.Listen("tcp", ":1234")
+	port_, _ := strconv.Atoi(args[1])
+	port = uint16(port_)
+
+	listener, err := net.Listen("tcp", ":" + args[1])
 
 	if err != nil {
 		log.Fatal("ListenTCP error:", err)
